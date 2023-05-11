512c83b9a4669469e0e20b95016576986829412b
PeterZabila <petrozabila@gmail.com>
2023-05-11 17:43:17 +0300

    added api request

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Stack, Box, Typography } from '@mui/material';
-import { Sidebar } from './';
+import { Sidebar, Videos } from './';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+  }, [selectedCategory])
+
   return (
     <Stack
       sx={{
@@ -10,7 +18,10 @@ const Feed = () => {
       }}
     >
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        <Sidebar/>
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
         <Typography className='copyright' variant="body2"
           sx={{ mt: 1.5, color: '#fff' }}
@@ -18,15 +29,16 @@ const Feed = () => {
           Copyright PZ 2023
         </Typography>
       </Box>
-      <Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2}
           sx={{ color: 'white' }}
         >
-          <span style={{ marginRight: '5px' }}>New</span>
+          <span style={{ marginRight: '5px' }}>{setSelectedCategory}</span>
           <span
             style={{ color: '#F31503', fontSize: '26px' }}
           >videos</span>
         </Typography>
+        <Videos videos={[]}/>
       </Box>
     </Stack>
   )
